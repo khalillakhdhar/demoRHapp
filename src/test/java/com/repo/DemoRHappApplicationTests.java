a151a56d38f07ba2ae4d3db099d2417e095fe35c
khalil <khalillakhdharatc@gmail.com>
2023-06-19 23:01:37 +0100

    Update DemoRHappApplicationTests.java

--- src/test/java/com/repo/DemoRHappApplicationTests.java
+++ src/test/java/com/repo/DemoRHappApplicationTests.java
@@ -1,11 +1,34 @@
 package com.repo;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoRHappApplicationTests {
-
+ValidateUser vu;
+@BeforeEach
+void init()
+{
+vu=new ValidateUser();	
+}
 	
+	
+	@Test
+	public void testValide()
+	{
+		String nom="Khalil";
+		assertTrue(vu.validateName(nom));
+		
+	}
+	@Test
+	public void testInvalide()
+	{
+		String nom="user";
+		assertTrue(vu.validateName(nom));
+		
+	}
 
 }
