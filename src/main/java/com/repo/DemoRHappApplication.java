8959a3887b70ade3199c5fca3b8a5c9a6262bb8b
khalil <khalillakhdharatc@gmail.com>
2023-06-19 22:41:28 +0100

    Initial commit

---
+++ src/main/java/com/repo/DemoRHappApplication.java
@@ -0,0 +1,13 @@
+package com.repo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRHappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoRHappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/repo/DemoRHappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.repo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoRHappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
